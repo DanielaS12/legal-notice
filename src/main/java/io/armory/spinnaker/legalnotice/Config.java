8b73dc027d9feacb374d3dfd6a9cbdfaa5a68003
DanielaS12 <111055962+DanielaS12@users.noreply.github.com>
2023-04-04 14:06:44 +0300

    Initial commit

---
+++ src/main/java/io/armory/spinnaker/legalnotice/Config.java
@@ -0,0 +1,12 @@
+package java.java.io.armory.spinnaker.legalnotice;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class Config {
+    private static final String LOGGER =  LoggerFactory.getLogger(Config.class);
+}
