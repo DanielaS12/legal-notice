7640a1412142269b11c7dfc620368d6f85c00c10
Ovidiu Popa <ovidiu.popa@armory.io>
2023-04-07 14:14:09 +0300

    fix: Add @PostConstruct

--- src/main/java/io/armory/legalnotice/Config.java
+++ src/main/java/io/armory/legalnotice/Config.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
+
 @Configuration
 public class Config {
     private static final Logger LOGGER =  LoggerFactory.getLogger(Config.class);
     private static final String legalNotice = "\n\n*************************************\n\nThis software is copyright of Armory, Inc. Any use of this software is subject to and governed by the Armory Terms and Conditions available at https://www.armory.io/terms-and-conditions/ and this software is considered part of Armory Services under those Terms and Conditions.\n\nYour continued use of this software acknowledges and accepts the Armory Terms and Conditions.\n\nUse of this software requires a valid license which can be obtained from Armory by contacting info@armory.io.\n\n*************************************\n\n";
 
-    @Bean
+    @PostConstruct
     public void logLegalNotice() {
         LOGGER.warn(legalNotice);
     }
